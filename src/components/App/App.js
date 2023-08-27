d29fccb5cea49271e92b4f526e5a77b9c6ab48d4
Bill Bitok <billylaroi@gmail.com>
2023-08-27 19:58:19 +0300

    Commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
 function App() {
-  return <h1>Bill's Portfolio</h1>;
+  return (
+    <div>
+      <div className="container">
+        <h1>Bill's Portfolio.</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Github</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
