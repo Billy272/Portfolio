819dd9d2529ae63651e08214bda1d181ee1d4673
Bill Bitok <billylaroi@gmail.com>
2023-08-30 09:17:54 +0300

    commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css'
 
 function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Name: ' + name);
+        console.log('Email: ' + email);
+        console.log('Message: ' + message);
+    };
+
     return (
         <div className="contact">
-            <h2>More Info</h2>
+            <h2></h2>
         </div>
     );
 }
