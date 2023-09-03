9a2ad2c419d77fce47569f50632862cdc13496fc
Bill Bitok <billylaroi@gmail.com>
2023-09-03 18:19:53 +0300

    commit

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -1,2 +1,16 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+
+function Header(props) {
+    return (
+        <header  className="header">
+            <div className="header-content">
+                <div className="text-content">
+                    <h1>{props.name}</h1>
+                    <p>{props.profession}</p>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Header;
