fba02d6056c8992b1bc7cac7b899008908b77296
Bill Bitok <billylaroi@gmail.com>
2023-09-04 12:27:21 +0300

    commit

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -5,6 +5,10 @@ function About() {
     return(
         <div className="about">
             <h2>About me</h2>
+            <p>
+                Bill Bitok here!. I am a passionate full-stack developer
+                and Junior Data Scientist. I enjoy coding and learning.
+            </p>
         </div>
     )
 }
