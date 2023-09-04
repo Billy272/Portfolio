d07bc0c1e15768f18c131076a3a195aa9d05769a
Bill Bitok <billylaroi@gmail.com>
2023-09-04 13:33:41 +0300

    commit

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Contact.css'
 
-function Contact(props) {
+function Contact() {
     return (
         <div className="contact">
             <h3>Contact Me</h3>
+            <p>
+                You can reach me for assistance,greetings or even job opportunity.
+            </p>
+            <a href="billcordias8@gmail.com">Send Email</a>
         </div>
     )
 }
