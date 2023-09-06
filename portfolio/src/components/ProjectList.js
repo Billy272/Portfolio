58b77a1cab38e78c8e224a0b835119ebcb6fef7f
Bill Bitok <billylaroi@gmail.com>
2023-09-06 18:16:48 +0300

    commit

--- portfolio/src/components/ProjectList.js
+++ portfolio/src/components/ProjectList.js
@@ -5,7 +5,7 @@ function ProjectList() {
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        fetch('${API_BASE_URL}/api/projects')
+        fetch('${API_BASE_URL}/api/projects/')
             .then(response => response.json())
             .then(data => setProjects(data))
             .data(error => console.error('Error fetching projects:', error));
