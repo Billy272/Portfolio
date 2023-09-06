21a35198a5ca4a2c566c6b4c67a93c03c88f389e
Bill Bitok <billylaroi@gmail.com>
2023-09-06 11:07:43 +0300

    commit

--- portfolio/src/components/ProjectList/ProjectList.js
+++ portfolio/src/components/ProjectList/ProjectList.js
@@ -1,13 +1,26 @@
 import React, {useState, useEffect} from "react";
-import API_BASE_URL from "../../config";
+import API_BASE_URL from "./config";
 
 function ProjectList() {
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        fetch('${API_BASE_URL}/backend/src/main/java/com/example/backend/controller/ProjectController.java')
+        fetch('${API_BASE_URL}/api/projects')
             .then(response => response.json())
             .then(data => setProjects(data))
             .data(error => console.error('Error fetching projects:', error));
-    })
+    }, []);
+
+    return (
+        <div>
+            <h1>Projects</h1>
+            <ul>
+                {projects.map(project => (
+                    <li key={project.id}>{project.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
+
+export default ProjectList;
