f11732d827a256f0fe5b3bf568f163f85bf29a65
Billy272 <billylaroi@gmail.com>
2024-01-09 12:42:41 +0300

    commit

--- portfolio/src/components/App/App.js
+++ portfolio/src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from '../Header/Header';
+import Me from '../Me/Me';
 import About from '../About/About';
 import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header name="Bill Bitok" profession="React Junior Developer" />
+      <Me />
       <About />
       <Projects />
       <Contact />
